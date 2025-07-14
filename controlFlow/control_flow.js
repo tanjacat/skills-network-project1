4a4849a3c79310d8cab4786ec564a6c1e6241111
tanjacat <tanjacat194@gmail.com>
2025-07-14 10:55:55 -0400

    Add ternary operator example for authentication status

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -45,4 +45,8 @@ switch (userType) {
 }
 console.log("User Category:", userCategory);
 
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
+
 
